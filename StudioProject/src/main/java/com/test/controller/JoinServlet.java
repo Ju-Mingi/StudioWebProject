75b0d234052a992a797abd8f4b686f8f732fbf7f
Ju-Mingi <jmg961218@gmail.com>
2021-07-08 17:51:27 +0900

    joinSerlvet 수정 및 회원정보수정페이지 설계 테스트

--- StudioProject/src/main/java/com/test/controller/JoinServlet.java
+++ StudioProject/src/main/java/com/test/controller/JoinServlet.java
@@ -17,48 +17,55 @@ import com.test.dto.MemberVO;
 @WebServlet("/join.do")
 public class JoinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public JoinServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public JoinServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String url = "member/joinForm.jsp";
 		// 회원가입 화면을 표시한다.
 		request.getRequestDispatcher(url).forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String password = request.getParameter("pw");
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
 		String birthday = request.getParameter("birth");
-		String email = request.getParameter("email_1")+"@"+request.getParameter("email_2");
-		String phone = request.getParameter("phone_1")+request.getParameter("phone_2")+request.getParameter("phone_3");
-		String address = request.getParameter("address_street")+request.getParameter("address_detail");
+		String email = request.getParameter("email_1") + "@" + request.getParameter("email_2");
+		String phone = request.getParameter("phone_1") + request.getParameter("phone_2")
+				+ request.getParameter("phone_3");
+		String address = request.getParameter("address_street") + request.getParameter("address_detail");
 		String question = request.getParameter("question");
 		String answer = request.getParameter("answer");
 		String ent_name = request.getParameter("ent_name");
-		String ent_num = request.getParameter("ent_num_1")+request.getParameter("ent_num_2")+request.getParameter("ent_num_3")+request.getParameter("ent_num_4");
-		String ent_phone = request.getParameter("ent_phone_1")+request.getParameter("ent_phone_2")+request.getParameter("ent_phone_3");
-		String ent_address = request.getParameter("ent_address_street")+request.getParameter("ent_address_detail");
+		String ent_num = request.getParameter("ent_num_1") + request.getParameter("ent_num_2")
+				+ request.getParameter("ent_num_3") + request.getParameter("ent_num_4");
+		String ent_phone = request.getParameter("ent_phone_1") + request.getParameter("ent_phone_2")
+				+ request.getParameter("ent_phone_3");
+		String ent_address = request.getParameter("ent_address_street") + request.getParameter("ent_address_detail");
 		int result = 0;
-		
+
 		// DB 연결 Instance 얻기
 		MemberDAO mDao = MemberDAO.getInstance();
-		
+
 		// joinForm 에서 가져온 파라미터 값을 MemberVO 에 담기
 		MemberVO mVo = new MemberVO();
 		mVo.setId(id);
@@ -75,16 +82,17 @@ public class JoinServlet extends HttpServlet {
 		mVo.setEnt_num(ent_num);
 		mVo.setEnt_phone(ent_phone);
 		mVo.setEnt_address(ent_address);
-		
+
 		// DB에 접속하여 회원정보 추가 메서드 실행
 		result = mDao.insertMember(mVo);
-		if(result == 1) {
+		if (result == 1) {
 			// 회원정보 추가 성공 시 True , 세션(id)을 얻어옴
 			HttpSession session = request.getSession();
 			session.setAttribute("id", id);
 		}
+
 		request.getRequestDispatcher("member/loginForm.jsp").forward(request, response);
-		
+
 	}
 
 }
